8d6d51ebcc9b28a37650c2c2ef7c1161bc4c9328
sk000f <mike@skaife.me>
2022-02-06 11:33:57 +0000

    create initial chart visualisation

--- repos.py
+++ repos.py
@@ -12,11 +12,23 @@ response = r.json()
 print(f"Status code: {r.status_code}")
 print(f"Total repositories: {response['total_count']}")
 
-# loop over all the returned repos
 items = response['items']
+repo_names, stars = [], []
 for item in items:
-    print(f"Name: {item['name']}")
-    print(f"Owner: {item['owner']['login']}")
-    print(f"Stars: {item['stargazers_count']}")
-    print(f"Repository: {item['html_url']}")
-    print(f"Description: {item['description']}")
+    repo_names.append(item['name'])
+    stars.append(item['stargazers_count'])
+
+data = [{
+  'type': 'bar',
+  'x': repo_names,
+  'y': stars
+}]
+
+graph_layout = {
+  'title': 'Popular Python Projects',
+  'xaxis': {'title': 'Repo'},
+  'yaxis': {'title': 'Stars'}
+}
+
+fig = {'data': data, 'layout': graph_layout}
+offline.plot(fig, filename='repos.html')
