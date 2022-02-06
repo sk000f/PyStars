6f7e990c4859d5911111f4b578ea3682f9521fc6
sk000f <mike@skaife.me>
2022-02-06 11:42:15 +0000

    styling chart

--- repos.py
+++ repos.py
@@ -21,7 +21,12 @@ for item in items:
 data = [{
   'type': 'bar',
   'x': repo_names,
-  'y': stars
+  'y': stars,
+  'marker': {
+    'color': 'rgb(60,100,150)',
+    'line': {'width': 1.5, 'color': 'rgb(25,25,25)'}
+  },
+  'opacity': 0.6
 }]
 
 graph_layout = {
